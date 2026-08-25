74fad9b12d369adbe7b5bf2f4ed365ad2012fbea
Allan Kivuva <kivuvaallan@gmail.com>
2026-08-25 15:18:49 -0300

    Add Navbar and Footer to PublicLayout component

--- frontend/src/layouts/PublicLayout.jsx
+++ frontend/src/layouts/PublicLayout.jsx
@@ -1,5 +1,15 @@
 import { Outlet } from 'react-router-dom'
+import Navbar from '../components/navigation/Navbar'
+import Footer from '../components/navigation/Footer'
 
 export default function PublicLayout() {
-  return <Outlet />
+  return (
+    <div className="public-layout">
+      <Navbar />
+      <main className="public-main">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
 }

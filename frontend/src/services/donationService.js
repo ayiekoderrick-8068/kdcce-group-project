18658ea4cb03b8cff0980a33e8bf05fe0b8dfdff
jer3myvv2 <jeremyleanan209@gmail.com>
2026-08-26 23:45:59 +0300

    Implement donation service functions

--- frontend/src/services/donationService.js
+++ frontend/src/services/donationService.js
@@ -1,1 +1,16 @@
-// TODO: implement.
+import api from './api'
+
+export async function createDonation(payload) {
+  const { data } = await api.post('/api/donations', payload)
+  return data.donation
+}
+
+export async function listDonations(params = {}) {
+  const { data } = await api.get('/api/donations', { params })
+  return data
+}
+
+export async function donationTotals() {
+  const { data } = await api.get('/api/donations/totals')
+  return data
+}
